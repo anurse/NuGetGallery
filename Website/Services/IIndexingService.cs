7b081d3e41e50aa000962e7a14e23e86cc905d03
pranavkm <prkishn@hotmail.com>
2011-11-29 18:04:22 -0800

    Draft implementation with Lucene

---
+++ Website/Services/IIndexingService.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace NuGetGallery
+{
+    public interface IIndexingService
+    {
+        void CreateIndex();
+
+        void UpdateIndex(Package package);
+
+        IEnumerable<int> Search(string term);
+    }
+}
